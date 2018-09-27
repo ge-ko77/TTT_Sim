4d129c17e0f5f0b37927e7621292b92928224998
Egg3234 <nospam@gmail.com>
2018-09-27 09:53:53 -0500

    Edits to TTT game object

--- TTT_Sim/src/ttt_Sim/TTT_Game.java
+++ TTT_Sim/src/ttt_Sim/TTT_Game.java
@@ -1,5 +1,16 @@
 package ttt_Sim;
+/*
+ * Samuel Giesbrecht
+ * (Luke Schroeder and Noah Voth)
+ * 
+ * 9-27-18
+ * 
+ * Tic Tac Toe Game object for TTT Simulation
+ */
+
+import java.util.Random;
 
 public class TTT_Game {
+	private int p1score, p2score;
 
 }
